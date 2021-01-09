9da2a04330ff25ad2ec58da5e6638c1184a551e6
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2021-01-09 16:37:55 -0600

    feat(pages/_app.tsx,pages/api/hello.ts,helpers/hooks.ts): typescript and fetch hook
    Transformed the components to use swr with hooks and typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import useSWR from 'swr'
 import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { fetcher } from '../helpers/hooks'
 
-function MyApp({ Component, pageProps }) {
+export default function Page({ Component, pageProps }: AppProps) {
+  const { data, error } = useSWR('http://localhost:3000/api/hello', fetcher)
+
+  if (error) return 'An error has occurred.'
+  if (!data) return 'Loading...'
+
   return <Component {...pageProps} />
 }
-
-export default MyApp
