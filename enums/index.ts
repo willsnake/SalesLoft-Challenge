43d6d08dd8da007eac4c6dd72f9d8cb5004427d1
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2021-01-10 16:44:44 -0600

    feat: created tests for API SalesLoft communication
    Added new interfaces for SalesLosft communication with the API and Class for different requests

---
+++ enums/index.ts
@@ -0,0 +1,20 @@
+export enum GetPeopleOptionsSortBy {
+  account = 'account',
+  cadence_stage_name = 'cadence_stage_name',
+  call_count = 'call_count',
+  clicked_emails = 'clicked_emails',
+  created_at = 'created_at',
+  job_seniority = 'job_seniority',
+  last_contacted_at = 'last_contacted_at',
+  name = 'name',
+  replied_emails = 'replied_emails',
+  sent_emails = 'sent_emails',
+  title = 'title',
+  updated_at = 'updated_at',
+  viewed_emails = 'viewed_emails',
+}
+
+export enum GetPeopleOptionsSortDirection {
+  ASC = 'ASC',
+  DESC = 'DESC',
+}
