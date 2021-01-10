4dae1bb1f3a1a2805520b9092889661c4184a331
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2021-01-10 16:45:22 -0600

    style(__tests__/index.spec.tsx): styling

--- __tests__/index.spec.tsx
+++ __tests__/index.spec.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
-import {shallow} from 'enzyme'
+import { shallow } from 'enzyme'
 import IndexPage from '../pages/index'
 
 describe('Pages', () => {
   describe('Index', () => {
     it('should render the Header with the Page Title', function () {
-      const wrap = shallow(<IndexPage/>)
-      expect(wrap.find('div').text()).toBe('<Head />SalesLoft Challenge by WillSnake')
+      const wrap = shallow(<IndexPage />)
+      expect(wrap.find('div').text()).toBe(
+        '<Head />SalesLoft Challenge by WillSnake'
+      )
     })
-  })  
+  })
 })
